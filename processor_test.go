0a7c2204c42e69e31004f3f6a6476e140399dc0f
krujos <jkruck@pivotal.io>
2015-06-08 09:17:34 -0500

    Add tests

---
+++ processor_test.go
@@ -0,0 +1,12 @@
+package exceptionprocessor_test
+
+import (
+	. "github.com/krujos/exceptionprocessor"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Processor", func() {
+
+})
